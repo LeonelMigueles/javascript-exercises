d6175de0f2cee3fa546926bb59fb47c4fddedcb2
Leonel Migueles <LeonelMigueles@gmail.com>
2024-12-04 17:35:20 -0500

    implemented with a for loop instead of repeat method

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -2,7 +2,12 @@ const repeatString = function(string, num) {
     if (num < 0) {
         return "Invalide input";
     }
-    return string.repeat(num);
+
+    let result = "";
+    for (let i = 0; i < num; i++) {
+       result += string; 
+    }
+    return result;
 };
 
 // Do not edit below this line
