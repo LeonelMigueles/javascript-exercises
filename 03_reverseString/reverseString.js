bcb7a4eee90753d102b2f505f4c863b17a34890b
Leonel Migueles <LeonelMigueles@gmail.com>
2024-12-04 17:51:10 -0500

    used a for loop but I should look up how to use split, reverse, join methonds

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let reverseString = "";
+    for (let i = 1; i <= string.length; i++) {
+        let lastChar = string[string.length - i];
+        reverseString += lastChar;
+    }
+    return reverseString;
 };
 
 // Do not edit below this line
